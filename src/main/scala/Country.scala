9de25f89fdf79350c430cfc13f8ba4b20612682f
Guillaume Hamel <>
2022-01-24 15:47:25 +0100

    Added optionality to country case class

--- src/main/scala/Country.scala
+++ src/main/scala/Country.scala
@@ -1,26 +1,27 @@
+import utils.Extensions._
+
 final case class Country (
                          id: String,
                          code: String,
                          name: String,
                          continent: String,
-                         wikipediaLink: String,
-                         keywords: String
+                         wikipediaLink: Option[String],
+                         keywords: Option[String]
                          ) {
 
   override def toString: String = {
-    s"\n$id - [$code] $name ($continent)".appendedAll(
-        if (!wikipediaLink.isBlank) s"\n    ↳ Wikipedia: $wikipediaLink" else "")
+    s"\n$id - [$code] $name ($continent)"
+      .appendedAll(if (wikipediaLink.isDefined) s"\n    ↳ Wikipedia: ${wikipediaLink.get}" else "")
+      .appendedAll(if (keywords.isDefined) s"\n    ↳ Keywords: ${keywords.get}" else "")
   }
 }
-
-
 
 object Country {
   def apply(input: String): Country = {
     val values = input.replaceAll("[\"]", "")
       .split(",", -1)
 
-    Country(values(0), values(1), values(2), values(3), values(4), values(5))
+    Country(values(0), values(1), values(2), values(3), values(4).noneIfBlank, values(5).noneIfBlank)
   }
 
 }
--- src/main/scala/utils/Extensions.scala
+++ src/main/scala/utils/Extensions.scala
@@ -2,8 +2,9 @@ package utils
 
 object Extensions {
   extension (str: String) {
-    def isValidDouble: Boolean = {
-      str.matches("(^(-*[0-9])+\\.[0-9]+$)")
-    }
+    def isValidDouble: Boolean = str.matches("(^(-*[0-9])+\\.[0-9]+$)")
+    
+    def noneIfBlank: Option[String] = if (str.isBlank) None else Some(str)
+  
   }
 }
