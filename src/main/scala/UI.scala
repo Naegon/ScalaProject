c9fbf1a439601f6b369f62e12d76db072c3559fb
Guillaume Hamel <>
2022-02-07 16:29:41 +0100

    Moved report logic inside dedicated methods

--- src/main/scala/UI.scala
+++ src/main/scala/UI.scala
@@ -83,74 +83,81 @@ object UI {
         val input = getUserInput(1 to 3, "Please choose one a number between 1 and 3")
 
         input match {
-            case 1 =>
-                val rawAirports = Parser.readFromFile("src/main/Resources/airports.csv").drop(1)
-                val airports = Parser.parseToAirport(rawAirports)
-                val rawCountries = Parser.readFromFile("src/main/Resources/countries.csv").drop(1)
-                val countries = Parser
-                  .parseToCountry(rawCountries)
-                  .map(x => (x._2, x._3))
-                  .toSeq
-
-                val airportByCountry = airports
-                  .flatten
-                  .groupBy(_.isoCountry)
-                  .view.mapValues(_.length)
-                  .toSeq
-                  .sortWith(_._2 > _._2)
-
-                val countriesMap = countries.toMap
-                val result = airportByCountry.map(x => (countriesMap.getOrElse(x._1, x._2), x._2))
-
-                val highestAirportNb = result.take(10)
-                val lowestAirportNb = result.slice(result.size - 10, result.size)
-
-                println("\n\nUpper:\n"+highestAirportNb.mkString("\n"))
-                println("\nLower:\n"+lowestAirportNb.mkString("\n"))
-
-// type of runways (surface) per country
-            case 2 =>
-                val rawCountries = Parser.readFromFile("src/main/Resources/countries.csv").drop(1)
-                val countries = Parser
-                  .parseToCountry(rawCountries)
-                  .map(x => (x._2, x._3))
-                  .toMap
-
-                val rawAirports = Parser.readFromFile("src/main/Resources/airports.csv").drop(1)
-                val airports = Parser
-                  .parseToAirport(rawAirports)
-                  .flatten
-                  .map(x => (x.isoCountry,x.ident))
-                  .map(x => (x._2,countries.getOrElse(x._1, x._2)))
-                  .toMap
-
-                val rawRunways = Parser.readFromFile("src/main/Resources/runways.csv").drop(1)
-                val runways = Parser
-                  .parseToRunways(rawRunways)
-                  .flatten
-                  .map(x => (x.airport_ident, x.surface))
-                  .map(x => (airports.getOrElse(x._1, None), x._2)) // should we find a method to rm lines where it doesn't exist?
-                  .map {
-                      case (x, y) if (x != None) && (y != "None") => (x, y) // if None in x or None in y  (there is surface named None in runways.csv)
-                      case (x, y) => None
-                  }
-                  // need problem to remove None, rm previous lines?
-                  .distinct
-
-                println(runways.mkString("\n"))
-
-            case 3 =>
-                val rawRunways = Parser.readFromFile("src/main/Resources/runways.csv").drop(1)
-                val runways = Parser
-                  .parseToRunways(rawRunways)
-                  .flatten
-                  .groupBy(_.le_ident)
-                  .view.mapValues(_.length)
-                  .toSeq
-                  .sortWith(_._2 > _._2)
-
-                println("\n10 most common runways latitude: \n%s".format(runways.take(10).mkString("\n")))
+            case 1 => topTen()
+            // type of runways (surface) per country
+            case 2 => runwaysPerCountry()
+            case 3 => topLatitude()
         }
+    }
+
+    def topTen(): Unit = {
+        val rawAirports = Parser.readFromFile("src/main/Resources/airports.csv").drop(1)
+        val airports = Parser.parseToAirport(rawAirports)
+        val rawCountries = Parser.readFromFile("src/main/Resources/countries.csv").drop(1)
+        val countries = Parser
+          .parseToCountry(rawCountries)
+          .map(x => (x._2, x._3))
+          .toSeq
+
+        val airportByCountry = airports
+          .flatten
+          .groupBy(_.isoCountry)
+          .view.mapValues(_.length)
+          .toSeq
+          .sortWith(_._2 > _._2)
+
+        val countriesMap = countries.toMap
+        val result = airportByCountry.map(x => (countriesMap.getOrElse(x._1, x._2), x._2))
+
+        val highestAirportNb = result.take(10)
+        val lowestAirportNb = result.slice(result.size - 10, result.size)
+
+        println("\n\nUpper:\n"+highestAirportNb.mkString("\n"))
+        println("\nLower:\n"+lowestAirportNb.mkString("\n"))
+    }
+
+    def runwaysPerCountry(): Unit = {
+        val rawCountries = Parser.readFromFile("src/main/Resources/countries.csv").drop(1)
+        val countries = Parser
+          .parseToCountry(rawCountries)
+          .map(x => (x._2, x._3))
+          .toMap
+
+        val rawAirports = Parser.readFromFile("src/main/Resources/airports.csv").drop(1)
+        val airports = Parser
+          .parseToAirport(rawAirports)
+          .flatten
+          .map(x => (x.isoCountry,x.ident))
+          .map(x => (x._2,countries.getOrElse(x._1, x._2)))
+          .toMap
+
+        val rawRunways = Parser.readFromFile("src/main/Resources/runways.csv").drop(1)
+        val runways = Parser
+          .parseToRunways(rawRunways)
+          .flatten
+          .map(x => (x.airport_ident, x.surface))
+          .map(x => (airports.getOrElse(x._1, None), x._2)) // should we find a method to rm lines where it doesn't exist?
+          .map {
+              case (x, y) if (x != None) && (y != "None") => (x, y) // if None in x or None in y  (there is surface named None in runways.csv)
+              case (x, y) => None
+          }
+          // need problem to remove None, rm previous lines?
+          .distinct
+
+        println(runways.mkString("\n"))
+    }
+
+    def topLatitude(): Unit = {
+        val rawRunways = Parser.readFromFile("src/main/Resources/runways.csv").drop(1)
+        val runways = Parser
+          .parseToRunways(rawRunways)
+          .flatten
+          .groupBy(_.le_ident)
+          .view.mapValues(_.length)
+          .toSeq
+          .sortWith(_._2 > _._2)
+
+        println("\n10 most common runways latitude: \n%s".format(runways.take(10).mkString("\n")))
     }
 
 }
