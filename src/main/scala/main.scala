3e12e349e457a50b0f1e69306fd40cdd125d9660
Guillaume Hamel <>
2022-02-07 15:31:59 +0100

    Removed unused class previously used for testing purposed

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,24 +8,4 @@ object main {
 
     menu()
   }
-
-  def getAndShowAirports(): Unit = {
-    val rawAirports = Parser.readFromFile("src/main/Ressources/airports.csv").drop(1)
-    val airports = Parser.parseToAirport(rawAirports)
-
-    println(airports.flatten.mkString("\n"))
-  }
-
-  def getAndShowCountries(): Unit = {
-    val rawCountries = Parser.readFromFile("src/main/Ressources/countries.csv").drop(1)
-    val countries = Parser.parseToCountry(rawCountries)
-    println(countries.mkString("\n"))
-  }
-
-  def getAndShowRunways(): Unit = {
-    val rawRunways = Parser.readFromFile("src/main/Ressources/runways.csv").drop(1)
-    val runways = Parser.parseToRunways(rawRunways)
-    println(runways.flatten.mkString("\n"))
-  }
-  
 }
